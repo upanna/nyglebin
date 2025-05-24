f1056f60eecc5d77d81d880575ae2bcae7cd8620
Upanna <145360237+upanna@users.noreply.github.com>
2025-05-24 08:52:04 +0630

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// in index.html's onAuthStateChanged
-if (user) {
-    // ... existing code ...
-    const myProfileLink = document.getElementById('my-profile-link');
-    if (myProfileLink) {
-        myProfileLink.href = `profile.html?uid=${user.uid}`;
-    }
-    // ...
-}
